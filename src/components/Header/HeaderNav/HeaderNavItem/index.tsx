c16d9f2290f135c7ae85498fc78b18ea9cd70454
Matheus Foresto Moselli <mat.moselli@hotmail.com>
2023-04-18 21:06:18 -0300

    schore: adding framer-motion

--- src/components/Header/HeaderNav/HeaderNavItem/index.tsx
+++ src/components/Header/HeaderNav/HeaderNavItem/index.tsx
@@ -1,9 +1,18 @@
 import { HeaderNavItemProps } from "./types";
+import { motion } from "framer-motion";
 
 export function HeaderNavItem(props: HeaderNavItemProps) {
     return (
-        <li>
-            <a href={props.href} className={`text-base px-4 py-3`}>{props.title}</a>
-        </li>
+        <motion.li 
+            whileHover={{ scale: 1.1 }} 
+            whileTap={{ scale: 0.9 }}
+        >
+            <a 
+                href={props.href} 
+                className={`text-base px-4 py-3`}
+            >
+                {props.title}
+            </a>
+        </motion.li>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import { Righteous } from "next/font/google"
 import { HeaderNav } from "./HeaderNav"
+import { motion } from "framer-motion";
 
 const righteous = Righteous({ weight: "400", subsets: ["latin"] })
 
 export function Header() {
     return (
         <header className="bg-gray-800 w-full flex flex-row items-center justify-between gap-2 px-8 py-6 h-24">
-            <h1 className={`text-3xl ${righteous.className}`}>Mtw</h1>
+            <motion.h1 
+                drag
+                dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+                className={`text-3xl ${righteous.className}`}
+            >
+                Mtw
+            </motion.h1>
 
             <HeaderNav />
         </header>
